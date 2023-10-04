d4259f9c260dd80d6546c9aa30057b77a5410311
radosttina <radosttina@users.noreply.github.com>
2023-10-04 10:49:03 +0300

    Initial commit

---
+++ launch-countdown/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<header>
+				<h1>Launch Countdown</h1>
+			</header>
+			<footer>
+				<div class="attribution">
+					Challenge by{" "}
+					<a
+						href="https://www.frontendmentor.io?ref=challenge"
+						rel="noreferrer"
+						target="_blank"
+					>
+						Frontend Mentor
+					</a>
+					. Coded by{" "}
+					<a href="https://github.com/radosttina">@radosttina</a>.
+				</div>
+			</footer>
+		</div>
+	);
+}
+
+export default App;
